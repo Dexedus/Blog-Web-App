28a27eb6656f800958e10ea7f140ac8ecca0f6cd
Dexedus <karlfleming64@gmail.com>
2024-10-21 16:17:36 +0100

    Added functionality for updating, deleteing and creating posts

--- server.js
+++ server.js
@@ -12,10 +12,65 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", (req,res)=>{
-  res.render("index.ejs")
+app.get("/", async (req,res)=>{
+  try{
+    const response = await axios.get(`${API_URL}/Posts`);
+    console.log(response);
+    res.render("index.ejs",{
+      posts: response.data,
+    });
+  } catch (error){
+    res.status(500).json({ message: "Error fetching posts" })  }
+});
+
+app.get("/new", (req,res)=>{
+  res.render("edit.ejs", {
+    heading: "New Post",
+  })
 })
 
+app.get("/edit/:id", async (req,res)=>{
+  try {
+    const response = await axios.get(`${API_URL}/posts/${req.params.id}`);
+    console.log(response.data)
+    res.render("edit.ejs",{
+      heading: "Update Post",
+      post: response.data,
+    })
+  } catch (error) {
+    res.status(500).json({ message: "Error Fetching Post"})
+  }
+})
+
+app.post("/api/posts", async (req,res)=>{
+  try{
+    const response = await axios.post(`${API_URL}/posts`, req.body);
+    console.log(response.data);
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).json({ message: "Error Creating Post" });
+  }
+});
+
+app.post("/api/posts/:id", async (req,res)=>{
+  console.log("called")
+  try{
+    const response = await axios.patch(`${API_URL}/posts/${req.params.id}`, req.body);
+    console.log(response.data);
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).json({ message: "Error Updating Post" });
+  }
+})
+
+app.get("/api/posts/delete/:id", async (req, res) => {
+  try {
+    await axios.delete(`${API_URL}/posts/${req.params.id}`);
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting post" });
+  }
+});
 
 
 
